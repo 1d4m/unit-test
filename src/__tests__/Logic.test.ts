f167c5da7491aee1548b219be04903263df356e1
daxchx <daxchx.v1@gmail.com>
2025-09-07 23:33:04 +0900

    test: ロジック/関数のテストコードを作成

---
+++ src/__tests__/Logic.test.ts
@@ -0,0 +1,21 @@
+import { formatDate } from "../util/formatDate";
+import { isEven } from "../util/isEven";
+import { sum } from "../util/sum";
+
+// ロジック関数のテスト
+describe("ロジックコンポーネント", () => {
+  it("sum関数", () => {
+    const total = sum(1, 2); // 期待値は3
+    expect(total).toBe(3);
+  });
+
+  it("isEven関数", () => {
+    expect(isEven(1)).toBe(false); // 1は偶数ではない
+    expect(isEven(2)).toBe(true); // 2は偶数である
+  });
+
+  it("formatDate関数", () => {
+    expect(formatDate(new Date(2025, 8, 7))).toBe("2025/09/07"); // Dateインスタンスが渡った場合
+    expect(formatDate("2025/12/1")).toBe("2025/12/01");
+  });
+});
