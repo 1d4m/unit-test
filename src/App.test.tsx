16b7205998f77a5e9e65262f3f7ff96754a4d7ae
daxchx <daxchx.v1@gmail.com>
2025-09-07 23:26:27 +0900

    delete: 不要ファイルの削除

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Counter } from "./components/Counter";
-
-describe("カウンター", () => {
-  it("初期値をチェック", () => {
-    render(<Counter />);
-    expect(screen.getByText("0")).toBeInTheDocument();
-  });
-});
--- src/components/Counter.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const Counter = () => {
-  const [count] = useState(0);
-
-  return <p>{count}</p>;
-};
