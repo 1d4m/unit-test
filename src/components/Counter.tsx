079ea2215e041e2f8990d0b6217af6602f2e8eaf
daxchx <daxchx.v1@gmail.com>
2025-09-08 23:46:00 +0900

    feat: countをreducerで管理

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,41 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+type Action = { type: "increment" } | { type: "decrement" };
+
+const reducer = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleClick = (action: "increment" | "decrement") => {
+    dispatch({ type: action });
   };
 
   return (
     <>
       <p aria-label="count">{count}</p>
-      <button type="button" onClick={handleClick}>
-        increment
+      <button
+        type="button"
+        aria-label="increment"
+        onClick={() => handleClick("increment")}
+      >
+        + increment
+      </button>
+      <button
+        type="button"
+        aria-label="decrement"
+        onClick={() => handleClick("decrement")}
+      >
+        - decrement
       </button>
     </>
   );
