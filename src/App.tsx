83a105736c28df4d9515e61c131c016b81ad3e91
daxchx <daxchx.v1@gmail.com>
2025-09-23 21:38:46 +0900

    feat: コンテキストを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { createContext } from "react";
 import { Counter } from "./components/Counter";
 import { LogicComponent } from "./components/Logic";
+import { DemoPage } from "./components/Theme";
+
+export const ThemeContext = createContext<string | null>(null);
 
 function App() {
   return (
     <>
       <LogicComponent />
       <Counter />
+      <DemoPage />
     </>
   );
 }
